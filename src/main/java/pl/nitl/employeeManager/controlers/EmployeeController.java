da9c434f40f9ec81a857e2cf03d4fa3acdc6eabb
Flemh <przemyslaw.niedorozow@gmail.com>
2019-11-08 22:18:59 +0100

    Blank lines formating

--- src/main/java/pl/nitl/employeeManager/controlers/EmployeeController.java
+++ src/main/java/pl/nitl/employeeManager/controlers/EmployeeController.java
@@ -1,6 +1,5 @@
 package pl.nitl.employeeManager.controlers;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.nitl.employeeManager.exceptions.EmployeeNotFoundException;
@@ -8,53 +7,40 @@ import pl.nitl.employeeManager.models.Employee;
 import pl.nitl.employeeManager.respositories.EmployeeRespository;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class EmployeeController {
-
     @Autowired
     EmployeeRespository repository;
 
-
-
-    //@PostMapping(path="/employees",consumes = {"application/xml"}) get produces, post consumes
-    @PostMapping("/employee")//@RequestBody potrzebne jest gdy chcemy przesylac bezposredniio json a nie za pomoca form
+    @PostMapping("/employee")
     public Employee addEmployee(@RequestBody Employee employee) {
         repository.save(employee);
         return employee;
     }
 
-    @PutMapping(path="/employee",consumes = {"application/json"})
+    @PutMapping(path = "/employee", consumes = {"application/json"})
     public Employee updateOrSaveEmployee(@RequestBody Employee employee) {
         repository.save(employee);
         return employee;
     }
 
-
     @DeleteMapping("/employee/{eid}")
-    public String deleteEmployee(@PathVariable int eid){
+    public String deleteEmployee(@PathVariable int eid) {
         Employee e = repository.getOne(eid);
         repository.delete(e);
         return "deleted";
     }
 
-
-
     @GetMapping("employees")
-    public List<Employee> getEmployees(){
-
-
+    public List<Employee> getEmployees() {
         return repository.findAll();
     }
 
     @GetMapping("employee/{eid}")
-    public Employee getEmployee(@PathVariable("eid") int eid){
-
-
+    public Employee getEmployee(@PathVariable("eid") int eid) {
         return repository.findById(eid)
                 .orElseThrow(() -> new EmployeeNotFoundException(eid));
     }
-
 
 }
