f2c39d50972f1c406dd1bd272656992efe40ac1d
Flemh <przemyslaw.niedorozow@gmail.com>
2019-11-11 00:51:08 +0100

    Add mapping Employee to EmployeeDto and EmployeeDto to Employee by mapStruct and testing it

--- src/main/java/pl/nitl/employeeManager/controlers/EmployeeController.java
+++ src/main/java/pl/nitl/employeeManager/controlers/EmployeeController.java
@@ -1,10 +1,13 @@
 package pl.nitl.employeeManager.controlers;
 
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.nitl.employeeManager.exceptions.EmployeeNotFoundException;
+import pl.nitl.employeeManager.mappers.EmployeeMapper;
 import pl.nitl.employeeManager.models.Employee;
 import pl.nitl.employeeManager.respositories.EmployeeRespository;
+import pl.nitl.employeeManager.views.EmployeeDto;
 
 import java.util.List;
 
@@ -12,24 +15,30 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     EmployeeRespository repository;
+    private EmployeeMapper mapper
+            = Mappers.getMapper(EmployeeMapper.class);
 
     @PostMapping("/employee")
-    public Employee addEmployee(@RequestBody Employee employee) {
-        System.out.println("adding new employee " + employee);
-        repository.save(employee);
-        return employee;
+    public Employee addEmployee(@RequestBody EmployeeDto employeeDto) {
+        Employee mappedEmployee = mapper.employeeDtoToEmployee(employeeDto);
+        System.out.println("adding new employee " + employeeDto + "and mapping it to " + mappedEmployee);
+        repository.save(mappedEmployee);
+        return mappedEmployee;
     }
 
     @PutMapping(path = "/employee", consumes = {"application/json"})
-    public Employee updateOrSaveEmployee(@RequestBody Employee employee) {
-        System.out.println("adding new employee " + employee);
-        repository.save(employee);
-        return employee;
+    public Employee updateOrSaveEmployee(@RequestBody EmployeeDto employeeDto) {
+        Employee mappedEmployee = mapper.employeeDtoToEmployee(employeeDto);
+        System.out.println("adding new employee " + employeeDto + "and mapping it to " + mappedEmployee);
+        repository.save(mappedEmployee);
+        return mappedEmployee;
     }
 
     @DeleteMapping("/employee/{eid}")
     public String deleteEmployee(@PathVariable int eid) {
-        Employee e = repository.getOne(eid);
+
+        Employee e = repository.findById(eid)
+                .orElseThrow(() -> new EmployeeNotFoundException(eid));
         System.out.println("adding new employee " + e);
         repository.delete(e);
         return "deleted";
